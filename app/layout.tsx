46d6e2479f4afead797773e00a36ba8888bb3c74
Aniruddh Gupta <aniruddhgupta5@example.comgit config --global user.name Aniruddh>
2026-04-24 12:18:33 +0530

    Fix favicon metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -79,21 +79,9 @@ export const metadata: Metadata = {
     canonical: 'https://www.wecode4u.com',
   },
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
+    icon: "/icon.png",
+    shortcut: "/icon.png",
+    apple: "/icon.png",
   },
 }
 
